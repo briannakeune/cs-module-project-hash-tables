b44631b958e270f920183383294caff51fd3d007
Brianna Keune <briannakeune@gmail.com>
2020-07-08 15:07:57 -0500

    word count passing

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,22 @@
-def word_count(s):
-    # Your code here
+import re
 
+
+def word_count(s):
+    characters_to_ignore = "[\"\&\.\:\;\,\-\+\=\/\\\[\]\{\}\(\)\*\^\&\|\s]"
+    cache = {}
+    words = re.sub(characters_to_ignore, ' ', s).lower().split()
+    for word in words:
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
+    print(word_count(":Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
